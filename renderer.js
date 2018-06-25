3a51da306e5a6a82b4902212572e47d88aa997c6
Hector Del Rosario Perez <hectordrp@MacBook-Pro-de-Hector.local>
2018-06-25 16:20:39 +0100

    Refactorizacion completada

--- renderer.js
+++ renderer.js
@@ -7,6 +7,8 @@ const accessToken = 'ec76c5b7cec544ee9076a2d7de995aea';
 // Create circuit SDK client instance
 const client = new Circuit.Client(config.sandbox.options);
 
+
+//Circuit Call function
 async function stream(user) {
     let conv = await client.getConversationById(config.sandbox.conversation);
     let call = await client.findCall(conv.rtcSessionId);
@@ -20,6 +22,7 @@ async function stream(user) {
 
 }
 
+//Circuit Video function
 async function videoStream(user) {
     let conv = await client.getConversationById(config.sandbox.conversation);
     let call = await client.findCall(conv.rtcSessionId);
@@ -59,120 +62,134 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Program (async IIFE function)
+// Bot features
 (async () => {
     // Logon
     const user = await client.logon();
     console.log(`Logged on as bot: ${user.emailAddress}`);
-    console.log(`bot data: ${JSON.stringify(user)}`);
+    //console.log(`bot data: ${JSON.stringify(user)}`);
 
+
+
+//Circuit Events
+
+    //Item Updated Event
     client.addEventListener('itemUpdated', function (itemUpdated) {
         console.log('Item Actualizado ->', itemUpdated);
     });
+
+    // Mention Event
     client.addEventListener('mention', function (mention) {
         console.log('Mencion ->', mention);
-    });
-    client.addEventListener('itemAdded', function (itemAdded) {
         // console.log(itemAdded.item.text.mentionedUsers.length);
-
         // for (let i = 0; i< itemAdded.item.text.mentionedUsers.length; i++) {
         //     console.log('index: ', i);
         // }
-        console.log('Texto detectado');
-        let dibotId = '5b345359-0bc6-49fd-893a-3e7e7da77208';
-        let convId = itemAdded.item.convId;
-        let creatorId = itemAdded.item.creatorId;
-        let itemId = itemAdded.item.itemId;
-        let parentId = itemAdded.item.parentItemId;
-        let content = itemAdded.item.text.content;
+    });
+
+    //Item Added Event:
+    client.addEventListener('itemAdded', function (itemAdded) {
+        console.log('Item nuevo -> ', itemAdded);
+        let itemConfig = {
+            dibotId: '5b345359-0bc6-49fd-893a-3e7e7da77208',
+            convId: itemAdded.item.convId,
+            creatorId: itemAdded.item.creatorId,
+            itemId: itemAdded.item.itemId,
+            parentId: itemAdded.item.parentItemId,
+            content: itemAdded.item.text.content
+        };
 
 
-        if (creatorId !== user.userId) {
-            if (manageCommands(content)) {
-                console.log('ok');
+        if (itemConfig.creatorId !== user.userId) {
+            if (manageCommands(itemConfig.content)) {
+                //console.log('ok');
             }
             else {
-                smallTalk();
+                smallTalk(itemConfig);
             }
         }
-
-
-
-
-        function manageCommands(content) {
-            if (content === '/daily') {
-                console.log('llamando');
-                // Start conference if not yet started/joined and start streaming
-                stream(user);
-
-                // Ensure call is always up and stream is sent:
-                //setInterval(async () => await stream(), 10 * 1000);
-                return true;
-
-            }
-            else if (content === '/exit') {
-                // Disconnect conference
-                console.log('Desconectando');
-                let conv = client.getConversationById(config.sandbox.conversation);
-                client.findCall(conv.rtcSessionId)
-                    .then((call = call) => {
-                        console.log(call);
-                        client.leaveConference(call.callId);
-                    });
-                return true;
-            }
-            else if (content === '/video') {
-                console.log('Video llamada');
-                // Start conference if not yet started/joined and start streaming
-                videoStream(user);
-
-                // Ensure call is always up and stream is sent.
-                //setInterval(async () => await stream(), 10 * 1000);
-                return true;
-            }
-        }
-
-        function selectId () {
-            if (parentId) {
-                return parentId;
-            } else {
-                return itemId;
-            }
-        }
-
-        function smallTalk() {
-            console.log('Mandando a Api');
-            let options = {
-                url: 'https://api.dialogflow.com/v1/query?v=20170712&query=' + content + '&lang=es&sessionId=eeb47626-8603-41e4-abee-ca59ed842bec&timezone=Atlantic/Canary',
-                headers: {
-                    "Authorization": "Bearer " + accessToken
-                }
-            };
-
-            console.log('request');
-            request(options, DialogFlowCallback);
-        }
-
-        function DialogFlowCallback(error, response, body) {
-            console.log('callback', response);
-            if (!error && response.statusCode === 200) {
-                let info = JSON.parse(body);
-                console.log("body: ", info);
-
-
-                let responseMessage = {
-                    parentId: selectId(),
-                    content: info.result.fulfillment.speech
-                };
-                if (responseMessage && responseMessage.content !== "") {
-                    client.addTextItem(convId, responseMessage)
-                        .then(console.log('text send -> ', responseMessage));
-                }
-            }
-        }
-
-
     });
+
+
+
+//Functions & Utils
+
+    //Command features
+    function manageCommands(content) {
+        if (content === '/daily') {
+            console.log('[Daily] -> Iniciando conferencia');
+            // Start conference if not yet started/joined and start streaming
+            stream(user);
+
+            // Uncomment line below to Ensure call is always up and stream is sent:
+            //setInterval(async () => await stream(), 10 * 1000);
+            return true;
+
+        }
+        else if (content === '/exit') {
+            // Disconnect conference
+            console.log('[Exit] -> Desconectando conferencia');
+            let conv = client.getConversationById(config.sandbox.conversation);
+            client.findCall(conv.rtcSessionId)
+                .then((call = call) => {
+                    console.log(call);
+                    client.leaveConference(call.callId);
+                });
+            return true;
+        }
+        else if (content === '/video') {
+            console.log('[Video] -> Iniciando Video-Conferencia');
+            // Start conference if not yet started/joined and start streaming
+            videoStream(user);
+
+            // Ensure call is always up and stream is sent.
+            //setInterval(async () => await stream(), 10 * 1000);
+            return true;
+        }
+    }
+
+    //Tell if the item is from a new topic
+    function selectId(itemConfig) {
+        if (itemConfig.parentId) {
+            //Has a parentId, so is not from a new topic
+            return itemConfig.parentId;
+        } else {
+            //New topic
+            return itemConfig.itemId;
+        }
+    }
+
+    //Call the DialogFlow Dibot SmallTalk api
+    function smallTalk(itemConfig) {
+        let options = {
+            url: 'https://api.dialogflow.com/v1/query?v=20170712&query=' + itemConfig.content + '&lang=es&sessionId=eeb47626-8603-41e4-abee-ca59ed842bec&timezone=Atlantic/Canary',
+            headers: {
+                "Authorization": "Bearer " + accessToken
+            }
+        };
+
+        //Request to DialogFlow, then manage the callback
+        request(options, DialogFlowCallback.bind(itemConfig));
+    }
+
+
+    //Callback with response message and status
+    function DialogFlowCallback(error, response, body) {
+        let itemConfig = this;
+        if (!error && response.statusCode === 200) {
+            let info = JSON.parse(body);
+            // console.log("Envío a DialogFlow -> ", info);
+
+            let responseMessage = {
+                parentId: selectId(itemConfig),
+                content: info.result.fulfillment.speech
+            };
+            if (responseMessage && responseMessage.content !== "") {
+                client.addTextItem(this.convId, responseMessage)
+                    .then(console.log('Respuesta DialogFlow -> ', responseMessage));
+            }
+        }
+    }
 })();
 
 // Print all events for debugging
